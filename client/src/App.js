8e86dd9e4369c2efe2925c4f789959a0be97dbb8
faithchep <faithchepkoech40@gmail.com>
2021-09-29 16:52:35 +0300

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -3,22 +3,38 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo"
-                     alt="logo" />
+class App extends Component {
+    state = {
+        data: null
+    };
 
-                <p>A simple React app.....</p>
+    componentDidMount() {
+        this.callBackendAPI()
+            .then(res => this.setState({ data: res.express }))
+            .catch(err => console.log(err));
+    }
+    // fetching the GET route from the Express server which matches the GET route from server.js
+    callBackendAPI = async () => {
+        const response = await fetch('/express_backend');
+        const body = await response.json();
 
-                <form action="../../post" method="post"
-                      className="form">
-                    <button type="submit">Connected?</button>
-                </form>
-            </header>
-        </div>
-    );
+        if (response.status !== 200) {
+            throw Error(body.message)
+        }
+        return body;
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">{this.state.data}</p>
+            </div>
+        );
+    }
 }
 
 export default App;
--- server.js
+++ server.js
@@ -5,13 +5,18 @@ const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-app.get('/express_backend', (req, res) => { //Line 9
-    res.send({ express: {
-            'method': 'GET',
-            'url': 'https://play.dhis2.org/2.34.6/api/dataElements',
-            'headers': {
-                'Authorization': 'Basic YWRtaW46ZGlzdHJpY3Q=',
-                'Cookie': 'JSESSIONID=BFBF45D4B6A8BAC4CF8525175396BDBA'
-            }
-        }}); //Line 10
+    app.get('/express_backend', (req, res) => { //Line 9
+        res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); //Line 10
+    })
+const options = {
+    'method': 'GET',
+    'url': 'https://play.dhis2.org/2.34.6/api/dataElements',
+    'headers': {
+        'Authorization': 'Basic YWRtaW46ZGlzdHJpY3Q=',
+        'Cookie': 'JSESSIONID=BFBF45D4B6A8BAC4CF8525175396BDBA'
+    }
+};
+request(options, function (error, response) {
+    if (error) throw new Error(error);
+    console.log(response.body);
 });
